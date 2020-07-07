72a86a01f7dcf646a7f8104086c4eff340092731
unknown <glhrmpaiva@gmail.com>
2020-07-07 13:22:45 -0300

    Finalizada pagina home estatica

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import {Pagina, BemVindo, Labe, Car, ImagemLogo, BotaoComprar, BotaoVender, Copyright} from './styles'
+
+
+
 
 export class AppContainer extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <Pagina>
+        <BemVindo>Bem-vindo a</BemVindo>
+        <Labe>Labe</Labe><Car>Car</Car>
+        <br />
+        <ImagemLogo src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT436uf0BuW2tMjhW5Ewv2MeMbsyz04GUEDNw&usqp=CAU'></ImagemLogo>
+        <br />
+        <BotaoComprar>Quero comprar</BotaoComprar>
+        <BotaoVender>Quero vender</BotaoVender>
+        <Copyright>©	Copyright	2020	Todos	os	direitos	reservados</Copyright>
+      </Pagina>
     )
   }
 }
---
+++ src/components/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+export const Pagina = styled.div`
+  text-align: center;
+  margin: 100px 0 0 0;
+`
+export const BemVindo = styled.h1`
+  margin-bottom: 0px;
+  font-size: 45px;
+`
+export const Labe = styled.span`
+  margin-top: -30px;
+  font-size: 72px;
+  font-weight: bold;
+  color: #4A6FC7;
+`
+export const Car = styled.span`
+  margin-top: -30px;
+  font-size: 72px;
+  font-weight: bold;
+  color: #C7AE71;
+`
+export const ImagemLogo = styled.img`
+  margin-top: 20px;
+`
+export const BotaoComprar = styled.button`
+  margin-right: 20px;
+  height: 50px;
+  width: 200px;
+  border-radius: 20px;
+  border: none;
+  background-color: #4A6FC7;
+  color: white;
+  box-shadow: 2px 2px 2px black;
+  font-weight: bold;
+  font-size: 20px;
+`
+export const BotaoVender = styled.button`
+  margin-left: 20px;
+  height: 50px;
+  width: 200px;
+  border-radius: 20px;
+  border: none;
+  background-color: #C7AE71;
+  color: white;
+  box-shadow: 2px 2px 2px black;
+  font-weight: bold;
+  font-size: 20px;
+`
+export const Copyright = styled.p`
+  bottom: 0;
+  position: absolute;
+  margin: 0 0 20px 20px;
+  font-size: 16px;
+`
